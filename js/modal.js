8a8e2c63466f8ae7e859536a592978c8a81df170
Victoria <vitagood.92@list.ru>
2023-10-05 22:08:24 +0300

    Внесение правок, разметка крукныз сеток, создание и стилизация модального окна

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const userNavigationButton = document.querySelector(".navigation-user-btn");
+const searchHotelsButton = document.querySelector(".searching-btn");
+const modalWindow = document.querySelector(".modal");
+const closeModalIcon = document.querySelector(".modal-close-icon");
+
+userNavigationButton.addEventListener("click", () => {
+  modalWindow.classList.add("open");
+});
+
+searchHotelsButton.addEventListener("click", () => {
+  modalWindow.classList.add("open");
+});
+
+closeModalIcon.addEventListener("click", () => {
+  modalWindow.classList.remove("open");
+});
