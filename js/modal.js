6f2094f7d88a98a85651217340a2ff2bb7d2eac5
Victoria <vitagood.92@list.ru>
2023-10-28 00:02:50 +0300

    Pixel perfect catalog

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,5 @@
 const userNavigationButton = document.querySelector('.navigation-user-btn');
-const searchHotelsButton = document.querySelector('.searching-btn');
+// const searchHotelsButton = document.querySelector('.searching-btn');
 const modalWindow = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('.modal-close-btn');
 
@@ -7,9 +7,9 @@ userNavigationButton.addEventListener('click', () => {
   modalWindow.classList.add('open');
 });
 
-searchHotelsButton.addEventListener('click', () => {
-  modalWindow.classList.add('open');
-});
+// searchHotelsButton.addEventListener('click', () => {
+//   modalWindow.classList.add('open');
+// });
 
 closeModalBtn.addEventListener('click', () => {
   modalWindow.classList.remove('open');
